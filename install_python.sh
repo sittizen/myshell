46e7f91b13995ec2e20a1b600c7e92976444947e
Simone Cittadini <sittizen@protonmail.com>
2020-03-30 11:01:52 +0200

    add tilix config step

---
+++ install_python.sh
@@ -0,0 +1,5 @@
+sudo apt install python3       
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+sudo apt install python3-pip
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
+
--- install_term_shell.sh
+++ install_term_shell.sh
@@ -10,6 +10,7 @@ set -g theme_date_format "+%H:%M"
 set -g theme_display_cmd_duration no
 
 
+echo "tilix preferences default profile command -> run as login shell"
 echo "tilix preferences default profile command -> custom shell /usr/bin/fish"
 echo "tilix preferences default profile color -> color scheme solarized dark"
 echo "tilix preferences default profile color -> don't use theme colors for fore/back"
