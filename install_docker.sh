c038b4a65ad05d13629a6a7bd0020c74a6fb0532
Simone Cittadini <sittizen@protonmail.com>
2020-04-14 12:16:24 +0200

    docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -1,2 +1,4 @@
 apt install uidmap
 su $1 bash get_docker.sh
+su $1 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o ~/.local/bin/docker-compose
+su $1 chmod +x ~/.local/bin/docker-compose
