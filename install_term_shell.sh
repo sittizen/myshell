7bfde25dcd3bb1b7548ce44570b06862d0c99fbc
Simone Cittadini <simone.cittadini@protonmail.com>
2020-04-17 09:12:46 +0200

    fixes in fish install

--- install_term_shell.sh
+++ install_term_shell.sh
@@ -1,4 +1,4 @@
-apt install tilix
+apt install tilix fish
 
 curl -L https://get.oh-my.fish | fish
 fish
@@ -8,6 +8,7 @@ git clone https://github.com/ryanoasis/nerd-fonts.git
 set -U theme_date_format "+%H:%M"
 set -U theme_display_cmd_duration no
 set -U DOCKER_HOST unix:///run/user/1000/docker.sock
+set -U fish_user_paths $HOME/.local/bin/
 mkdir ~/.config/fish/completions
 wget https://raw.github.com/barnybug/docker-fish-completion/master/docker.fish -O ~/.config/fish/completions/docker.fish
 
