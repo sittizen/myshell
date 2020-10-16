99cbc16684e85d0831a3c85df6b5b3da35aefcf1
simone cittadini <simone.cittadini@gruppomol.it>
2020-10-16 10:17:13 +0200

    add vault

--- 00_install_essentials.sh
+++ 00_install_essentials.sh
@@ -1,3 +1,6 @@
 apt-get update
 apt-get install htop docker-ce docker-ce-cli lazydocker
 
+wget https://releases.hashicorp.com/vault/1.5.4/vault_1.5.4_linux_amd64.zip
+unzip vault_1.5.4_linux_amd64.zip
+mv vault /usr/local/bin
