aa96e992ca72a1d50f363a5e754d4d68a076a2ad
simone cittadini <sittizen@protonmail.com>
2022-04-05 15:01:15 +0200

    Update 05_install_utils.sh

--- 05_install_utils.sh
+++ 05_install_utils.sh
@@ -1,2 +1,5 @@
-sudo apt-get install v4l-utils
-sudo npm install -g git-standup      
+sudo apt-get install v4l-utils bat
+sudo npm install -g git-standup
+cd /usr/bin
+sudo mv cat cat.bu
+ln -s batcat cat
