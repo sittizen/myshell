f00149c95a12fefdc7dcc699482ce351ea325eb6
simone cittadini <sittizen@protonmail.com>
2022-11-09 15:02:04 +0100

    Update 00_install_essentials.sh

--- 00_install_essentials.sh
+++ 00_install_essentials.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install htop docker-ce docker-ce-cli lazydocker
+apt-get install htop docker-ce docker-ce-cli lazydocker bat
 
 wget https://releases.hashicorp.com/vault/1.5.4/vault_1.5.4_linux_amd64.zip
 unzip vault_1.5.4_linux_amd64.zip
