4d7cf958c89d79e1b15817f5149b676a6ad7ec4f
simone cittadini <sittizen@protonmail.com>
2024-01-10 07:39:53 +0100

    Update 06_install_keychron.sh

--- 06_install_keychron.sh
+++ 06_install_keychron.sh
@@ -1,7 +1,8 @@
-echo "options hid_apple fnmode=0" | sudo tee /etc/modprobe.d/hid_apple.conf
+echo "options hid_apple fnmode=0" | tee /etc/modprobe.d/hid_apple.conf
 # 0 = disabled: Disable the 'fn' key. Pressing 'fn'+'F8' will behave like you only press 'F8'
-sudo update-initramfs -u -k all
-sudo systemctl reboot
+update-initramfs -u -k all
+systemctl reboot
+kbdrate -r 12
 
 # /etc/bluetooth/main.conf
 # FastConnectable = true
