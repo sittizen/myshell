193f6a0e417c2b2ed67c48a46814053f400bcc40
simone cittadini <sittizen@protonmail.com>
2024-04-04 12:26:21 +0200

    add glab to utils

--- 05_install_utils.sh
+++ 05_install_utils.sh
@@ -3,3 +3,5 @@ sudo npm install -g git-standup
 cd /usr/bin
 sudo mv cat cat.bu
 ln -s batcat cat
+wget https://gitlab.com/gitlab-org/cli/-/releases/v1.37.0/downloads/glab_1.37.0_Linux_x86_64.deb
+sudo dpkg -i glab_1.37.0_Linux_x86_64.deb
