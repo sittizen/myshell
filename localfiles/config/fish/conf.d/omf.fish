857effc3be23032bdf0a6209a164cf105007aa9d
Simone Cittadini <simone.cittadini@moltiplygroup.com>
2025-11-25 10:24:24 +0100

    quicker

--- localfiles/config/fish/conf.d/omf.fish
+++ localfiles/config/fish/conf.d/omf.fish
@@ -3,3 +3,6 @@ set -q XDG_DATA_HOME
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 source $OMF_PATH/init.fish
+
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_git_worktree_support yes
---
+++ localfiles/config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/quicker.nvim",
+	ft = "qf",
+	---@module "quicker"
+	---@type quicker.SetupOptions
+	opts = {},
+	config = function(_, opts)
+		require("quicker").setup({
+			keys = {
+				{
+					">",
+					function()
+						require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+					end,
+					desc = "Expand quickfix context",
+				},
+				{
+					"<",
+					function()
+						require("quicker").collapse()
+					end,
+					desc = "Collapse quickfix context",
+				},
+			},
+		})
+	end,
+}
